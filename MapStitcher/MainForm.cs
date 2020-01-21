a31622c81085e78643deb8c730eb2f963b9790d2
bp2008 <bp2008@users.noreply.github.com>
2020-01-21 09:38:24 -0700

    Updated default map API endpoint

--- MapStitcher/MainForm.cs
+++ MapStitcher/MainForm.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -665,5 +666,29 @@ namespace MapStitcher
 		{
 			SaveSettings();
 		}
+
+		#region Image Format Identification
+		private static Dictionary<byte[], ImageFormat> imageFormatIdentificationMap = new Dictionary<byte[], ImageFormat>()
+		{
+			{ new byte[] { 0x42, 0x4D }, ImageFormat.Bmp },
+			{ new byte[] { 0x47, 0x49, 0x46, 0x38, 0x37, 0x61 }, ImageFormat.Gif },
+			{ new byte[] { 0x47, 0x49, 0x46, 0x38, 0x39, 0x61 }, ImageFormat.Gif },
+			{ new byte[] { 0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A }, ImageFormat.Png},
+			{ new byte[] { 0xff, 0xd8 }, ImageFormat.Jpeg },
+		};
+		public static ImageFormat GetFormat(byte[] imageData)
+		{
+			return imageFormatIdentificationMap.FirstOrDefault(kvp => StartsWith(imageData, kvp.Key)).Value;
+		}
+		private static bool StartsWith(byte[] a, byte[] b)
+		{
+			if (a == null || a.Length < b.Length)
+				return false;
+			for (int i = 0; i < b.Length; i += 1)
+				if (a[i] != b[i])
+					return false;
+			return true;
+		}
+		#endregion
 	}
 }
--- MapStitcher/Settings.cs
+++ MapStitcher/Settings.cs
@@ -9,7 +9,7 @@ namespace MapStitcher
 {
 	public class Settings : SerializableObjectBase
 	{
-		public string BaseURL = "http://khm0.googleapis.com/kh?v=800&hl=en-US&x={X}&y={Y}&z={Z}";
+		public string BaseURL = "https://khms0.googleapis.com/kh?v=862&hl=en-US&x={X}&y={Y}&z={Z}";
 		public decimal startLat = 47.6215M;
 		public decimal startLon = -122.352M;
 		public decimal endLat = 47.6195M;
